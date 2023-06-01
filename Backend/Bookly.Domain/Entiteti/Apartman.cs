357c3c83527003063ffeec99b00f8d5c860bf781
Stefan Djurdjevic <stefke2002@gmail.com>
2023-06-01 21:20:42 +0200

    Inicijalizovan entity framework za domensku relacionu bazu podataka

---
+++ Backend/Bookly.Domain/Entiteti/Apartman.cs
@@ -0,0 +1,8 @@
+﻿using Bookly.Domain.Entiteti.Bazni;
+
+namespace Bookly.Domain.Entiteti;
+
+public class Apartman : Entitet<Guid>
+{
+    public string Naziv { get; private set; }
+}
---
+++ Backend/Bookly.Domain/Entiteti/Bazni/Entitet.cs
@@ -0,0 +1,14 @@
+﻿namespace Bookly.Domain.Entiteti.Bazni;
+
+public class Entitet<TId>
+{
+    public TId ID { get; private set; }
+
+    protected Entitet(TId id)
+    {
+        ID = id;
+    }
+    protected Entitet()
+    {
+    }
+}
