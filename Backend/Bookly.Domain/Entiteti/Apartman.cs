58a6c6c740a43833a2e8b6d135e750a44ee46b3f
grga023 <ognjen.grgur12@gmail.com>
2023-06-04 17:52:22 +0200

    Apps attributes

--- Backend/Bookly.Domain/Entiteti/Apartman.cs
+++ Backend/Bookly.Domain/Entiteti/Apartman.cs
@@ -5,4 +5,9 @@ namespace Bookly.Domain.Entiteti;
 public class Apartman : Entitet<Guid>
 {
     public string Naziv { get; private set; }
+    public string Mesto { get; private set; }
+    public string Drzava { get; private set; }
+    public double Cena { get; private set; }
+    public double Ocena { get; private set; }
+    public string Opis { get; private set; }
 }
