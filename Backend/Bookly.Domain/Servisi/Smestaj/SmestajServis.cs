4559097a7d1258985535507730bf7160dc913b7f
grga023 <ognjen.grgur12@gmail.com>
2023-06-04 18:34:21 +0200

    Add apartments

---
+++ Backend/Bookly.Domain/Servisi/Smestaj/SmestajServis.cs
@@ -0,0 +1,35 @@
+﻿using Bookly.Domain.Apstrakcije.Baza;
+using Bookly.Domain.Servisi.Smestaj.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bookly.Domain.Servisi.Smestaj
+{
+    public class SmestajServis
+    {
+        private readonly IAplikacioniDbContext _aplikacioniDbContext;
+
+        public SmestajServis(IAplikacioniDbContext aplikacioniDbContext)
+        {
+            _aplikacioniDbContext = aplikacioniDbContext;
+        }
+        
+        public async Task DodavanjeSmestajaAsync(NoviSmestajDTO noviSmestaj)
+        {
+            Guid noviApartmanId = Guid.NewGuid();
+            Entiteti.Apartman apartmanZaDodati = new(id: noviApartmanId,
+                                                       naziv: noviSmestaj.Naziv,
+                                                       mesto: noviSmestaj.Mesto,
+                                                       drzava: noviSmestaj.Drzava,
+                                                       cena: noviSmestaj.Cena,
+                                                       ocena: noviSmestaj.Ocena,
+                                                       opis: noviSmestaj.Opis);
+
+            _aplikacioniDbContext.Apartmani.Add(apartmanZaDodati);
+            _ = _aplikacioniDbContext.SaveChangesAsync();
+        }
+    }
+}
---
+++ Backend/Bookly.Infrastructure.Identity/Migrations/20230604163253_da.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bookly.Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class da : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
