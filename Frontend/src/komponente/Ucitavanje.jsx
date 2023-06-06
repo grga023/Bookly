a7bbf85e84738552961d824a0fcfa5763cb23a05
Dusan Lukic <dusanlukic404@gmail.com>
2023-06-06 15:05:10 +0200

    Dodat loading efekat na pocetnoj stranici

---
+++ Frontend/src/komponente/Ucitavanje.jsx
@@ -0,0 +1,22 @@
+export default function Ucitavanje() {
+  return (
+    <div>
+        <div className="w-full group overflow-hidden">
+          <div className="w-full bg-slate-500"></div>
+        </div>
+        <div className="p-4">
+          <div className="flex justify-between items-centar">
+            <h2 className="bg-slate-500 h-6"></h2>
+            <span className="bg-slate-500 h-4 w-50"></span>
+          </div>
+          <div>
+            <p className="my-3 bg-slate-500 h-4"></p>
+            <div>
+              <p className="h-4 bg-slate-500"></p>
+              <p className="h-4 bg-slate-500"></p>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
