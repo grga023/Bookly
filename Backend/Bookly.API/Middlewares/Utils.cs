f39340e627e9e824946184fe0fabcb403e83938d
Stefan Djurdjevic <stefke2002@gmail.com>
2023-06-10 18:24:54 +0200

    Dodat exception middleware

---
+++ Backend/Bookly.API/Middlewares/Utils.cs
@@ -0,0 +1,22 @@
+﻿using System.Net;
+using System.Net.Mime;
+
+namespace Bookly.API.Middlewares;
+
+internal static class Utils
+{
+    internal static HttpStatusCode ExceptionToStatusCode(this Exception exception)
+        => exception switch
+        {
+            ArgumentException => HttpStatusCode.BadRequest,
+            KeyNotFoundException => HttpStatusCode.NotFound,
+            _ => HttpStatusCode.InternalServerError
+        };
+
+    internal static async Task WriteJsonToHttpResponseAsync<TResponse>(HttpResponse httpResponse, HttpStatusCode statusCode, TResponse response)
+    {
+        httpResponse.ContentType = MediaTypeNames.Application.Json;
+        httpResponse.StatusCode = (int)statusCode;
+        await httpResponse.WriteAsJsonAsync(response);
+    }
+}
